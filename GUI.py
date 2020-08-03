276fb0c1ac125832062d9984e1da477008266d1c
Mael Lhoutellier <mlhoutel@gmail.com>
2020-08-03 17:07:18 +0200

    Kivy Gui initialisation

---
+++ GUI.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+
+Window.softinput_mode = "below_target"
+
+# Custom classes implementation
+class BackgroundColor(Widget):
+	pass
+class BackgroundLabel(Label,BackgroundColor):
+	pass
+class Message(BackgroundLabel):
+	pass
+class LeftMessage(Message):
+	pass
+class RightMessage(Message):
+	pass
+	
+class AppLayout(BoxLayout):
+	def sendMessage(self):
+		msg=self.ids.textbox.text
+		if msg:
+			self.ids.messages.add_widget(LeftMessage(text=msg))
+			self.ids.textbox.text=''
+
+class ChatApp(App):
+        def build(self):
+        	self.load_kv('app.kv')
+        	return AppLayout()
+ 
+if __name__ == "__main__":
+	ChatApp().run()

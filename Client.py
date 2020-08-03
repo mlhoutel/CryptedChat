87b93d0ae77f17e84f8f182eefcba020d4e9bece
Mael Lhoutellier <mlhoutel@gmail.com>
2020-08-03 13:12:17 +0200

    server and client basis

--- Client.py
+++ Client.py
@@ -1,5 +1,3 @@
-# Python program to implement client side of chat room. 
-
 import socket
 import select 
 import sys   
@@ -14,28 +12,27 @@ Port = int(sys.argv[2])
 server.connect((IP_address, Port)) 
 print("Connected:",IP_address, Port)
 
-while True: 
-    # maintains a list of possible input streams 
+while True:
     sockets_list = [sys.stdin, server] 
-
-    """ There are two possible input situations. Either the 
-    user wants to give  manual input to send to other people, 
-    or the server is sending a message  to be printed on the 
-    screen. Select returns from sockets_list, the stream that 
-    is reader for input. So for example, if the server wants 
-    to send a message, then the if condition will hold true 
-    below.If the user wants to send a message, the else 
-    condition will evaluate as true"""
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+    read_sockets, write_socket, error_socket = select.select(sockets_list,[],[]) 
 
     for socks in read_sockets: 
         if socks == server: 
-            message = socks.recv(2048) 
-            print(message)
+            data = socks.recv(2048)
+            if data:
+                message = data.decode('utf-8','ignore')
+                print(message)
+            else:
+                server.close()
+                exit()
         else: 
-            message = sys.stdin.readline() 
-            server.send(message) 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
+            text = sys.stdin.readline() 
+            message = text.rstrip()
+            if message == 'exit':
+                server.close()
+                exit()
+            server.sendall(message.encode('utf-8'))
+            # sys.stdout.write("<You>") 
+            # sys.stdout.write(message) 
             sys.stdout.flush() 
 server.close() 
